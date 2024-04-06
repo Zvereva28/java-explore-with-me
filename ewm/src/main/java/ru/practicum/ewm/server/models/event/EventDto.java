339bd200eda5e8c82f18e72660fc7800584f42dc
VTB4053680@region.vtb.ru <zverevaeg@vtb.ru>
2024-04-06 23:05:33 +0300

    feat: функционал работает

--- ewm/src/main/java/ru/practicum/ewm/server/models/event/EventDto.java
+++ ewm/src/main/java/ru/practicum/ewm/server/models/event/EventDto.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 import ru.practicum.ewm.server.exceptions.Marker;
 import ru.practicum.ewm.server.models.location.LocationDto;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -42,6 +43,7 @@ public class EventDto {
 
     private Boolean requestModeration = true;
 
+    @Min(0)
     private Integer participantLimit = 0;
 
 }
--- ewm/src/main/java/ru/practicum/ewm/server/models/event/UpdateEventUserRequest.java
+++ ewm/src/main/java/ru/practicum/ewm/server/models/event/UpdateEventUserRequest.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import ru.practicum.ewm.server.models.location.LocationDto;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.Size;
 
 @Getter
@@ -31,6 +32,7 @@ public class UpdateEventUserRequest {
 
     private Boolean paid;
 
+    @Min(0)
     private Integer participantLimit;
 
     private Boolean requestModeration;
