6502f6ee643109335e4c8d3dfd31b831bcd035d4
VTB4053680@region.vtb.ru <zverevaeg@vtb.ru>
2024-04-13 13:00:13 +0300

    fix: Исправила AdminCategoriesController

--- ewm/src/main/java/ru/practicum/ewm/server/controllers/PublicCategoriesController.java
+++ ewm/src/main/java/ru/practicum/ewm/server/controllers/PublicCategoriesController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import ru.practicum.ewm.server.models.category.Category;
 import ru.practicum.ewm.server.models.category.CategoryDto;
 import ru.practicum.ewm.server.service.CategoriesService;
 
